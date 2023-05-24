ae71ebcc4a875bb822485592e80dbec998258f58
kirillprogs <kirillopervussini@gmail.com>
2023-05-24 16:59:44 +0300

    Initial hierarchy

---
+++ include/Header.h
@@ -0,0 +1,7 @@
+
+#ifndef OPTOCUS_HEADER_H
+#define OPTOCUS_HEADER_H
+
+void function() { }
+
+#endif //OPTOCUS_HEADER_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include "Header.h"
+
 int main(int argc, char **argv) {
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
     button.show();
+    function();
     return QApplication::exec();
 }
